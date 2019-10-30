de438ef4bd096a31e3ceee68d4b41524ffc8aebe
kaetahr <a00825945@itesm.mx>
2019-10-30 00:26:09 -0600

    cleanup

--- tst.py
+++ tst.py
@@ -2,28 +2,10 @@ import plotly.offline as pyo
 import plotly_express as pyx
 import plotly.graph_objects as go
 import pandas as pd
-
+import plotly.figure_factory as ff
 data = pd.read_csv("sample_semanai.csv")
 
 dato_hist = pyx.histogram(data, x="Clave platillo")
-# dato_hist.show()
-
-# stacked_bar = go.Figure(data=[
-#     go.Bar(name="HAMBURGUESAS", x=data["Día"], y=data["Grupo"]),
-#     go.Bar(name="ENTRADAS", x=data["Día"], y=data["Grupo"]),
-#     go.Bar(name="BEBIDAS SIN ALCOHOL", x=data["Día"], y=data["Grupo"]),
-#     go.Bar(name="BEBIDAS CON ALCOHOL", x=data["Día"], y=data["Grupo"]),
-#     go.Bar(name="POSTRES", x=data["Día"], y=data["Grupo"]),
-#     go.Bar(name="CLÁSICOS", x=data["Día"], y=data["Grupo"]),
-#     go.Bar(name="PAPAS, PAPAS Y MÁS PAPAS", x=data["Día"], y=data["Grupo"]),
-#     go.Bar(name="BÁSICOS", x=data["Día"], y=data["Grupo"]),
-#     go.Bar(name="PASTAS", x=data["Día"], y=data["Grupo"]),
-#     go.Bar(name="EXTRAS COCINA", x=data["Día"], y=data["Grupo"])
-#     go.Bar(name="EXTRAS COCINA", x=data["Día"], y=data["Grupo"])
-# ])
-
-# stacked_bar.update_layout(barmode='stack')
-# stacked_bar.show()
 
 
 data_ham = data[data["Tipo de grupo"] == "Alimentos"]
@@ -33,3 +15,5 @@ ham_hist = pyx.histogram(data_ham, x="Clave platillo")
 data_bebidas = data[data["Tipo de grupo"] == "Bebidas"]
 bebidas_hist = pyx.histogram(data_bebidas, x="Clave platillo")
 bebidas_hist.show()
+
+fig = ff.create_dendrogram()
